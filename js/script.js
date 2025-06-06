248d4764bd3809683aa6941e39b7a4044eab7c11
Alejosantos007 <alejosantos.2024@gmail.com>
2025-06-06 20:53:43 -0300

    codiguito de weas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let disco = [{
+    name: 'Juan',
+    year: '1990',
+    songs: ['song1', 'song2', 'song3']
+},
+{
+    name: 'Brandon',
+    year: '2006',
+    songs: ['song4', 'song5', 'song6']
+}]
